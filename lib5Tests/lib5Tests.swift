7dd15fb319ddcd335123a7f8ab2f71d74f560728
yaoxin <yaoxin@sunlands.com>
2019-08-19 19:19:03 +0800

    Initial Commit

---
+++ lib5Tests/lib5Tests.swift
@@ -0,0 +1,34 @@
+//
+//  lib5Tests.swift
+//  lib5Tests
+//
+//  Created by y on 2019/8/19.
+//  Copyright © 2019 y. All rights reserved.
+//
+
+import XCTest
+@testable import lib5
+
+class lib5Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
